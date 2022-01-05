0f0a337db48c723d0a4e028a5abf680c7e45967f
prey52 <leon98765432100@gmail.com>
2022-01-05 14:39:56 +0100

    public

--- public/index.php
+++ public/index.php
@@ -1,57 +1,33 @@
-<?php
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
-
-/*dupa testing*/
+    <body>
+        <header>
+            <div class="header-left">
+                <p>O nas</p>
+                <p>Blog</p>
+                <p>Koszyk</p>
+            </div>
+            <!--<img src="logo.jpg">-->
+            <div class="header-right">
+                <img class="downl-img" src="android.png">
+                <img class="downl-img" src="apple.png">
+            </div>
+        </header>
+        <div class="search-bar">
+            <button class="submit"><img src="lupa.jpg"></button>
+            <input class="search" type="text" value="Szukaj...">
+            <div class="vl"></div>
+            <button class="search-category" value="wszystkie">Wszystkie</button>
+            <button class="search-category" value="wszystkie">Wojenne</button>
+            <button class="search-category" value="wszystkie">Strzelanki</button>
+            <button class="search-category" value="wszystkie">Wyścigi</button>
+            <button class="search-category" value="wszystkie">Przygodowe</button>
+            <button class="search-category" value="wszystkie">Bijatyki</button>
+            <button class="search-category" value="wszystkie">jRPG</button>
+            <div class="vl"></div>
+        </div>
+    </body>
+</html>
